4b5f89ebc807bd6e1aec1bbbd8186d6fd4b1f378
Vilan Jaywardene <vilanjayawardene0987@gmail.com>
2023-11-09 13:57:10 +0530

    testing cocotb

---
+++ test.py
@@ -0,0 +1,24 @@
+# CocoTB Projects
+
+Welcome to the CocoTB Projects repository! This collection features various hardware testing projects using CocoTB.
+
+## Projects
+
+1. **Project Name 1**
+   - Description: Brief overview.
+   - [Link to Project 1](link/to/project1)
+
+2. **Project Name 2**
+   - Description: Another exciting project.
+   - [Link to Project 2](link/to/project2)
+
+   ...
+
+## Getting Started
+
+1. Clone the repository and navigate to the project.
+2. Install CocoTB: `pip install cocotb`
+3. Run the project: `make test`
+
+Explore, contribute, and happy testing!
+
